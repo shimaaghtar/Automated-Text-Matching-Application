b1e9c82bb749688b33338872190419ef8f3b3d8c
Shima Aghtar <shima.aghtar@gmail.com>
2025-03-16 05:35:22 -0400

    Update exception.py

--- src/exception/exception.py
+++ src/exception/exception.py
@@ -1,17 +1,4 @@
 
-''''
-class ProjectException(Exception):
-    def __init__(self,error_message,error_datails:sys):
-        self.error_message = error_message
-        _,_,exec_tb = error_datails.exc_info()
-
-        self.lineno = exec_tb.tb_lineno
-        self.file_name = exec_tb.tb_frame.f_code.co_filename
-
-    def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-''' 
 import sys
 
 class ProjectException(Exception):
